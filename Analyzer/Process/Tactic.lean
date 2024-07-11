1b5fb9e0be6aa8e2ae3d0e270acd057a0a646053
kokic <3250679033@qq.com>
2024-07-11 11:08:43 +0800

    feat(tactic): add simp used theorems
    * feat: add simp used theorems
    
    * feat: add `dsimp` used theorems
    
    * chore: code style & simplify some implemetations
    
    ---------
    
    Co-authored-by: Tony Beta Lambda <tonybetalambda@gmail.com>

--- Analyzer/Process/Tactic.lean
+++ Analyzer/Process/Tactic.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2024 BICMR@PKU. All rights reserved.
 Released under the Apache 2.0 license as described in the file LICENSE.
-Authors: Tony Beta Lambda
+Authors: Tony Beta Lambda, Kokic
 -/
 import Lean
 import Analyzer.Types
@@ -31,14 +31,62 @@ partial def references : Syntax → HashSet Name
 def onLoad : CommandElabM Unit :=
   enableInfoTree
 
+open Elab.Tactic
+
+namespace Simp
+
+def getSimpStats (stx : Syntax) : TacticM Simp.Stats := withMainContext do
+  let { ctx, simprocs, dischargeWrapper } ← mkSimpContext stx (eraseLocal := false)
+  dischargeWrapper.with fun discharge? =>
+    simpLocation ctx simprocs discharge? (expandOptLocation stx[5])
+
+def getSimpAllStats (stx : Syntax) : TacticM Simp.Stats := withMainContext do
+  let { ctx, simprocs, .. } ← mkSimpContext stx (eraseLocal := true) (kind := .simpAll) (ignoreStarArg := true)
+  let (_, stats) ← simpAll (← getMainGoal) ctx (simprocs := simprocs)
+  return stats
+
+def getDSimpStats (stx : Syntax) : TacticM Simp.Stats := withMainContext do
+  let { ctx, simprocs, .. } ← mkSimpContext stx (eraseLocal := false) (kind := .dsimp)
+  let (_, stats) ← dsimpGoal (← getMainGoal) ctx (simprocs := simprocs)
+  return stats
+
+def getStats (stx : Syntax) : TacticM Simp.Stats :=
+  match stx.getKind with
+  | ``Parser.Tactic.simpAll => getSimpAllStats stx
+  | ``Parser.Tactic.dsimp => getDSimpStats stx
+  | _ => getSimpStats stx
+
+def getUsedTheorems (ci : ContextInfo) (ti : TacticInfo) : CommandElabM Json := do
+    if ti.stx.isOfKind |> List.any [
+      ``Parser.Tactic.simp,
+      ``Parser.Tactic.simpAll,
+      ``Parser.Tactic.dsimp,
+    ] then
+      let simpStats ← { ci with mctx := ti.mctxBefore }.runMetaM {} <|
+        getStats ti.stx
+        { elaborator := .anonymous } |>.run' { goals := ti.goalsBefore }
+        |>.run'
+      let usedTheorems := simpStats.usedTheorems.foldl (init := #[]) fun a k _ => a.push k.key
+      return json% {
+        usedTheorems: $(usedTheorems)
+      }
+    else
+      return .null
+
+end Simp
+
 def getResult : CommandElabM (Array TacticRunInfo) := do
   let info := (← getInfoTrees).foldl (init := #[]) fun info tree => tree.foldInfo collectTacticInfo info
   info.mapM fun (ci, ti) => do
+    let mut extra : Json := .null
+    extra := extra.mergeObj (← Simp.getUsedTheorems ci ti)
+
     return {
       tactic := ti.stx,
       references := references ti.stx,
       before := ← Goal.fromInfoBefore ci ti,
       after := ← Goal.fromInfoAfter ci ti,
+      extra? := if extra.isNull then none else extra,
     }
 
 end Analyzer.Process.Tactic
