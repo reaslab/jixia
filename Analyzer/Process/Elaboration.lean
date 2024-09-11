e897528fcef8bba58d2cc3e7142274a3387e809a
Meow <example@localhost>
2024-09-11 17:47:10 +0800

    feat: Dependency structure of metavariables

--- Analyzer/Process/Elaboration.lean
+++ Analyzer/Process/Elaboration.lean
@@ -1,12 +1,12 @@
 /-
 Copyright (c) 2024 BICMR@PKU. All rights reserved.
 Released under the Apache 2.0 license as described in the file LICENSE.
-Authors: Tony Beta Lambda, Kokic
+Authors: Tony Beta Lambda, Kokic, Znssong
 -/
 import Lean
 import Analyzer.Process.Tactic.Simp
 
-open Lean Elab Meta Command
+open Lean Elab Meta Command Tactic TacticM
 
 namespace Analyzer.Process.Elaboration
 open Analyzer.Process.Tactic
@@ -27,38 +27,78 @@ partial def references : Syntax → HashSet Name
   | .atom _ _ => .empty
   | .ident _ _ name _ => .empty |>.insert name
 
-def getUsedVariables (e : Expr) : MetaM (Array Name) :=
-  e.collectFVars.run default <&> fun s => s.2.fvarIds |>.map FVarId.name
+def getUsedFVarIds (e : Expr) : MetaM (Array FVarId) :=
+  e.collectFVars.run default <&> fun s => s.2.fvarIds
+
+def getUsedVariables (e : Expr) : MetaM (Array Name) := do
+  return (← getUsedFVarIds e).map FVarId.name
 
 def onLoad : CommandElabM Unit :=
   enableInfoTree
+
+def getDependentsFromGoal (goal : MVarId) : MetaM (Array MVarId × Array FVarId) := do
+  let mut visited : HashSet MVarId := {}
+  let mut mvars := #[goal]
+  let mut dependentMVars := #[]
+  let mut dependentFVars := #[]
+  while !mvars.isEmpty do
+    let mvarId := mvars.back
+    mvars := mvars.pop
+    if visited.contains mvarId then continue
+    visited := visited.insert mvarId
+    match ← getDelayedMVarAssignment? mvarId with
+    | none => do
+      match ← getExprMVarAssignment? mvarId with
+      | none => dependentMVars := dependentMVars.push mvarId
+      | some expr => mvars := mvars.append (← getMVars expr)
+    | some {fvars, mvarIdPending} => do
+      dependentFVars := dependentFVars.append <| fvars.map Expr.fvarId!
+      mvars := mvars.push mvarIdPending
+  return (dependentMVars, dependentFVars)
 
 def getResult : CommandElabM (Array TacticElabInfo) := do
   let trees ← getInfoTrees
   trees.toArray.concatMapM fun tree => do
     let info := tree.foldInfo (init := #[]) collectTacticInfo
-    if let some (ci, _) := info[0]? then
-      let mctx := ci.mctx
-      let getUsedInfo (mvar : MVarId) : MetaM (Option Json) := do
-        let typeUses ← getUsedVariables (← mvar.getType)
-        let valueUses ← withMCtx mctx <| getUsedVariables <| .mvar mvar
-        return json%{
-          typeUses: $(typeUses),
-          valueUses: $(valueUses)
-        }
+    if info.size == 0 then return #[]
+    let getUsedInfo (mvar : MVarId) : MetaM (Option Json) := do
+      let typeUses ← getUsedVariables (← mvar.getType)
+      let valueUses ← getUsedVariables <| .mvar mvar
+      let typeMVars := (← getMVars (← mvar.getType)).map MVarId.name
+      return json%{
+        typeUses: $(typeUses),
+        typeMVars: $(typeMVars),
+        valueUses: $(valueUses)
+      }
 
-      info.mapM fun (ci, ti) => do
-        let mut extra : Json := .null
-        extra := extra.mergeObj (← Simp.getUsedTheorems ci ti)
+    info.mapM fun (ci, ti) => do
+      let mut extra : Json := .null
+      extra := extra.mergeObj (← Simp.getUsedTheorems ci ti)
+      let goalsBefore ← runWithInfoBefore ci ti getUnsolvedGoals
+      let dependencies ← runWithInfoAfter ci ti do
+        goalsBefore.foldlM
+          fun map goal => do
+            match ← getExprMVarAssignment? goal with
+            | some _ =>
+              let (mvars, fvars) ← getDependentsFromGoal goal
+              let json := json%{
+                newGoals: $(mvars.map MVarId.name),
+                newHypotheses: $(fvars.map FVarId.name)
+              }
+              return HashMap.insert map goal json
+            | none => return map
+          ({} : HashMap MVarId Json)
 
-        pure {
-          tactic := ti.stx,
-          references := references ti.stx,
-          before := ← Goal.fromTactic (extraFun := getUsedInfo) |>.runWithInfoBefore ci ti,
-          after := ← Goal.fromTactic (extraFun := getUsedInfo) |>.runWithInfoAfter ci ti,
-          extra? := if extra.isNull then none else extra,
-        : TacticElabInfo}
-    else
-      pure #[]
+      let getUsedInfo' (mvar : MVarId) : MetaM (Option Json) := do
+        let extra ← getUsedInfo mvar
+        return do return .mergeObj (← extra) (← dependencies.find? mvar)
+
+      pure {
+        tactic := ti.stx,
+        references := references ti.stx,
+        before := ← Goal.fromTactic (extraFun := getUsedInfo') |>.runWithInfoBefore ci ti,
+        after := ← Goal.fromTactic (extraFun := getUsedInfo) |>.runWithInfoAfter ci ti,
+        extra? := if extra.isNull then none else extra,
+      : TacticElabInfo}
 
 end Analyzer.Process.Elaboration
