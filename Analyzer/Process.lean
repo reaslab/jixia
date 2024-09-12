774d6bbd0ab1bee593b4460d3504b2ccd0fdd12d
Tony Beta Lambda <tonybetalambda@gmail.com>
2024-09-12 13:51:56 +0800

    refactor: enforce plugins to be CommandElabM

--- Analyzer/Process.lean
+++ Analyzer/Process.lean
@@ -48,34 +48,22 @@ elab "impl_onLoad" : term => do
   let type ← `(Options → CommandElabM Unit)
   elabTerm term (← elabTerm type none)
 
-def hasHead (expr : Expr) (name : Name) : MetaM Bool :=
-  withNewMCtxDepth do
-    let mvar ← mkFreshExprMVar (Expr.sort 1)
-    isDefEq expr (← mkAppM name #[mvar])
-
 elab "impl_process" : term => do
   let param ← mkFreshBinderName
   let body ← Process.plugins.mapM fun (name, plugin) => do
     let cond := mkIdent (param ++ name ++ (Name.mkSimple "isPresent"))
     let action := mkIdent (param ++ name ++ (Name.mkSimple "output"))
-    let getResult := plugin.getResult
-    let type := (← getEnv).find? getResult |>.get!.type
-    let term ← if ← hasHead type ``CommandElabM then
-      `(runCommandElabM $(mkIdent getResult))
-    else if ← hasHead type ``FrontendM then
-      `($(mkIdent getResult))
-    else
-      throwError "getResult must be either CommandElabM or FrontendM"
+    let term := mkIdent plugin.getResult
     return (← `(doSeqItem| if $cond then
       $action:term (← $term)
     ))
   let term ← `(fun $(mkIdent param) => do $body*)
-  let type ← `(Options → FrontendM Unit)
+  let type ← `(Options → CommandElabM Unit)
   elabTerm term (← elabTerm type none)
 
 def run (options : Options) : FrontendM Unit := do
   runCommandElabM <| impl_onLoad options
   processCommands
-  impl_process options
+  runCommandElabM <| impl_process options
 
 end Analyzer
